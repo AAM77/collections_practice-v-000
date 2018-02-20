e1efbf47cc670821e155d20e60f27fb2f6afcbe7
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-20 18:43:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,6 +44,7 @@ end
 
 #returns the sum of all the numbers in the array
 def sum_array(array)
+  array.inject(0){|result_memo, element| result_memo + element}
   sum = 0
   array.each {|num| sum = sum + num}
   return sum
