f72fe42855fbf035067d950a2f0cd9a17ca58798
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-20 16:40:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+#sort in ascending order
+def sort_array_asc(array)
+  array.sort {|a,b| a <=> b}
+end
 
+#sort in descending order
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+#sort by string length (ascending)
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+#swaps the second and third elements
+def swap_elements(array)
+  array.insert(1, array.delete_at(2))
+end
